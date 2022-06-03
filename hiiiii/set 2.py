cfe1aaff3e6742de66e7eab02f4dd716687340a4
Ram Ji Dixit <100121942+Ramjidixit@users.noreply.github.com>
2022-06-03 20:29:06 +0530

    Delete hiiiii directory

--- hiiiii/set 2.py
+++
@@ -1,7 +0,0 @@
-#1 
-a=(275000*30*4*15)/100
-g=a/4
-b=g/30
-c=int(input("Enter the number of months in which growth takes place: "))
-d=c*30
-print("Total number of passengers=",275000*365+d*b)
--- hiiiii/set 4.py
+++
@@ -1,4 +0,0 @@
-#1
-a=input("Enter thw comma separated words:")
-words=[word for word in a.split(",")]
-print(",".join(sorted(list(set(words)))))
